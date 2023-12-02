2b2cb570dfe096e0abe00a6490aff2b7399cdddb
Davidson Tadeu <30707045+Davidson031@users.noreply.github.com>
2023-12-01 21:43:54 -0300

    abstracting survey and dashboard components into pagecomponent

--- react/src/views/Dashboard.jsx
+++ react/src/views/Dashboard.jsx
@@ -8,6 +8,7 @@ export default function Dashboard() {
             </header>
             <main>
                 <div className="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
+                    Dashboard content
                 </div>
             </main>
         </>
