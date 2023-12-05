4e718a34dfd4a2c9750b1b1d2cb7483dc4353eae
Davidson Tadeu <30707045+Davidson031@users.noreply.github.com>
2023-12-04 21:43:50 -0300

    TButton

--- react/src/views/Surveys.jsx
+++ react/src/views/Surveys.jsx
@@ -7,11 +7,15 @@ export default function Surveys() {
 
     const { surveys } = useStateContext();
 
+    const onDeleteClick = () => {
+        console.log("i am clicked");
+    }
+
     return (<PageComponent>
         <div>
             <div className="grid grid-cols-1 gap-5 sm:grid-cols-2 md:grid-cols-3">
                 {surveys.map((survey) => (
-                    <SurveyListItem survey={survey} key={survey.id} />
+                    <SurveyListItem survey={survey} key={survey.id} onDeleteClick={ onDeleteClick } />
                 ))}
             </div>
         </div>
