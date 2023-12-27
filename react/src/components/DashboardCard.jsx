3dbd92ae3e4cde7b104e8890150e588f8be5b3bf
Davidson Tadeu <30707045+Davidson031@users.noreply.github.com>
2023-12-27 17:48:47 -0300

    dashboard cards dynamic

--- react/src/components/DashboardCard.jsx
+++ react/src/components/DashboardCard.jsx
@@ -1,7 +1,7 @@
 
-export default function DashboardCard({ title, children }) {
+export default function DashboardCard({ title, children, style = '', className='' }) {
     return (
-        <div className="bg-white shadow-md p-3 text-center flex flex-col animate-fade-in-down">
+        <div className={"bg-white shadow-md p-3 text-center flex flex-col animate-fade-in-down" + className} style={ style }  >
             {
                 title &&
                 <h3 className="text-2xl font-semibold">
